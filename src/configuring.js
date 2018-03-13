2087504769412a8025a7cc18f9df58ef1bc3ab23
Jam Risser <jam@jamrizzi.com>
2018-03-13 01:06:05 +0100

    Fixed source path exists

--- src/configuring.js
+++ src/configuring.js
@@ -1,9 +1,10 @@
 import glob from 'glob';
+import path from 'path';
 
 export default async function configuring(yo) {
   yo.destinationRoot(yo.context.destination);
   const sourceFiles = glob.sync(
-    `${yo.context.sourcePath}/*.${yo.context.template}`
+    `${path.resolve('docs', yo.context.sourcePath)}/*.${yo.context.template}`
   );
   yo.context.sourceFilesExist = sourceFiles.length > 0;
 }
